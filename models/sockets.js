884ffbb50364c83de672b2de893b64ac9735d214
Moises Astudillo <bruma_moy@hotmail.com>
2021-10-10 05:10:45 -0500

    Socket disconnect event was added

--- models/sockets.js
+++ models/sockets.js
@@ -20,6 +20,13 @@ class Sockets {
                 socket.emit('hey-frontend', { ...payload, 'backendResponse': 'Hi Front!' });
             })
 
+            socket.on("disconnect", () => {
+                let numberOfClientsConnected =  this.getNumberOfClientsConnected()
+                if(numberOfClientsConnected <= 0){
+                    this.stopSendingDataByIntervas();
+                }
+            });
+
             this.startToSendingDataByIntervals();
         });
     }
@@ -27,17 +34,32 @@ class Sockets {
     startToSendingDataByIntervals() {
         if (!this.isSendingDataByIntervals) {
             this.isSendingDataByIntervals = true;
-            this.sendingDataByIntervalsIdentifier = setInterval(() => this.sendingDataByIntervals(), 1000);
+            this.sendingDataByIntervalsIdentifier = setInterval(() => this.sendingDataByIntervals(), 1500);
         }
     }
 
     sendingDataByIntervals() {
-        this.io.emit('hey-frontend', { 'backendResponse': 'Hi Front!' });
+        this.io.emit('hey-frontend', {
+            'Red': this.getRandomInt(),
+            'Blue': this.getRandomInt(),
+            'Yellow': this.getRandomInt(),
+            'Green': this.getRandomInt(),
+            'Purple': this.getRandomInt(),
+            'Orange': this.getRandomInt()
+        });
     }
 
     stopSendingDataByIntervas() {
         this.isSendingDataByIntervals = false;
         clearInterval(this.sendingDataByIntervalsIdentifier);
+    }
+
+    getRandomInt() {
+        return Math.floor(Math.random() * (100 - 1)) + 1;
+    }
+
+    getNumberOfClientsConnected(){
+        return this.io.engine.clientsCount;
     }
 }
 
